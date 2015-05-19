e2c41a96ac7a593d76cabe00f5d6ebd69427243d
Dale Bustad <dale@divmain.com>
2015-05-19 00:31:18 -0700

    Use relative paths in example app requires.

--- example/app/entry-a.js
+++ example/app/entry-a.js
@@ -1,5 +1,5 @@
-var libA = require("shared/lib-a");
-var libB = require("shared/lib-b");
+var libA = require("./shared/lib-a");
+var libB = require("./shared/lib-b");
 
 console.log("entry-a:libA", libA);
 console.log("entra-a:libB", libB);

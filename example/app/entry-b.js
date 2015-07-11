0aaf8361c391ae9c60df06cec991c4afc7ea6397
Dale Bustad <dale@divmain.com>
2015-07-11 00:57:15 -0700

    Transform require stmts with Babel; remove esquery; support ES6 modules.

--- example/app/entry-b.js
+++ example/app/entry-b.js
@@ -1,5 +1,5 @@
 var libB = require("./shared/lib-b");
-var libC = require("./shared/lib-c");
+import libC from "./shared/lib-c";
 
 console.log("entry-b:libB", libB);
 console.log("entry-b:libC", libC);

630ef7c72c38e8dddc6a11963fbe793335285ae9
Dale Bustad <dale@divmain.com>
2015-08-31 17:33:15 -0700

    Correct loading when require'd by other packages.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+var useTranspiled = true; // eslint-disable-line no-var
+
 try {
-  module.exports = require("./lib");
+  require.resolve("./lib");
 } catch (e) {
+  useTranspiled = false;
+}
+
+if (useTranspiled) {
+  require("babel/polyfill");
+  module.exports = require("./lib");
+} else {
   require("babel/register");
   module.exports = require("./src");
 }

ecc7a526d38fdd2d2f79b5f7f3c7159aa52bd0e2
Dale Bustad <dale@divmain.com>
2015-09-24 03:17:26 -0700

    Apply minor getBundles refactor.

--- src/compile/index.js
+++ src/compile/index.js
@@ -36,12 +36,16 @@ export const getModuleMaps = pluggable(function getModuleMaps (moduleSeeds) {
 }, { compileModules });
 
 export const getBundles = pluggable(function getBundles (moduleSeeds, moduleMaps) {
+  function getModulesFromHashes (bundle) {
+    return Object.assign({}, bundle, {
+      modules: bundle.moduleHashes.map(hash => moduleMaps.byHash[hash])
+    });
+  }
+
   return this.getBundleSeeds(moduleSeeds, moduleMaps.byAbsPath)
     .then(seedBundles => this.dedupeExplicit(seedBundles, moduleMaps.byAbsPath))
     .then(this.dedupeImplicit)
-    .then(bundles => bundles.map(bundle => Object.assign({}, bundle, {
-      modules: bundle.moduleHashes.map(hash => moduleMaps.byHash[hash])
-    })))
+    .then(bundles => bundles.map(getModulesFromHashes))
     .then(bundles => Promise.all(bundles.map(this.hashBundle)))
     .then(bundles => Promise.all(bundles.map(this.interpolateFilename)));
 }, { getBundleSeeds, dedupeExplicit, dedupeImplicit, hashBundle, interpolateFilename });

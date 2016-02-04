6fab960cef27224794bfa876155651e674e8e375
Dale Bustad <dale@divmain.com>
2016-02-03 17:09:24 -0800

    Formatting.

--- docs/compilation-vis.js
+++ docs/compilation-vis.js
@@ -1,14 +1,14 @@
 var md = markdownit();
 
 var width = 700,
-    height = 700,
-    radius = Math.min(width, height) / 2;
+  height = 700,
+  radius = Math.min(width, height) / 2;
 
 var x = d3.scale.linear()
-    .range([0, 2 * Math.PI]);
+  .range([0, 2 * Math.PI]);
 
 var y = d3.scale.sqrt()
-    .range([0, radius]);
+  .range([0, radius]);
 
 var color = d3.scale.category20c();
 
@@ -29,13 +29,13 @@ var tooltip = d3.select("body")
 var doc = d3.select("#doc");
 
 var partition = d3.layout.partition()
-    .value(function(d) { return d.size; });
+  .value(function(d) { return d.size; });
 
 var arc = d3.svg.arc()
-    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
-    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+  .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+  .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+  .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+  .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
 
 function updateToolTip (d) {
   tooltip.html(function () {
@@ -50,20 +50,20 @@ d3.json("./compilation.json", function(error, root) {
   if (error) throw error;
 
   var path = svg.selectAll("path")
-      .data(partition.nodes(root))
+    .data(partition.nodes(root))
     .enter().append("path")
-      .attr("d", arc)
-      .style("fill", function (d) { return color((d.children ? d : d.parent).name); })
-      .on("click", click)
-      .on("mouseover", updateToolTip)
-      .on("mousemove", function (d) {
-        return tooltip
-          .style("top", (d3.event.pageY-10)+"px")
-          .style("left", (d3.event.pageX+10)+"px");
-      })
-      .on("mouseout", function (){
-        return tooltip.style("opacity", 0);
-      });
+    .attr("d", arc)
+    .style("fill", function (d) { return color((d.children ? d : d.parent).name); })
+    .on("click", click)
+    .on("mouseover", updateToolTip)
+    .on("mousemove", function (d) {
+      return tooltip
+        .style("top", (d3.event.pageY-10)+"px")
+        .style("left", (d3.event.pageX+10)+"px");
+    })
+    .on("mouseout", function (){
+      return tooltip.style("opacity", 0);
+    });
 
   function click(d) {
     path.transition()
@@ -77,11 +77,11 @@ d3.json("./compilation.json", function(error, root) {
 
 function arcTween(d) {
   var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
-      yd = d3.interpolate(y.domain(), [d.y, 1]),
-      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+    yd = d3.interpolate(y.domain(), [d.y, 1]),
+    yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
   return function(d, i) {
-    return i
-        ? function(t) { return arc(d); }
-        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+    return i ?
+      function(t) { return arc(d); } :
+      function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
   };
 }

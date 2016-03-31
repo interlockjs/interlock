20d233946035b209b35e9607f8e29190a6df4e08
Dale Bustad <dale@divmain.com>
2016-03-31 16:18:26 -0700

    Lint.

--- src/options/shared.js
+++ src/options/shared.js
@@ -3,8 +3,8 @@ export function getLogger (verbosity) {
   return {
     error: (...msgs) => console.log(...msgs) || true,
     warn: verbosity >= 1 ? (...msgs) => console.log(...msgs) || true : () => false,
-    info: verbosity >= 2 ? (...msgs) => console.log(...msgs) || true: () => false,
-    debug: verbosity >= 3 ? (...msgs) => console.log(...msgs) || true: () => false
+    info: verbosity >= 2 ? (...msgs) => console.log(...msgs) || true : () => false,
+    debug: verbosity >= 3 ? (...msgs) => console.log(...msgs) || true : () => false
   };
 }
 /* eslint-enable no-console */

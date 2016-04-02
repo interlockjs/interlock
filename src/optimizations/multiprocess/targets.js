55357af6f364552bb8898fa84eb910089ea4d738
Dale Bustad <dale@divmain.com>
2016-04-01 23:40:41 -0700

    Add `parseModule` and `updateRequires` to multiprocess optimization.

--- src/optimizations/multiprocess/targets.js
+++ src/optimizations/multiprocess/targets.js
@@ -1,1 +1,3 @@
 export { default as transformModule } from "../../compile/modules/transform";
+export { default as parseModule } from "../../compile/modules/parse";
+export { default as updateRequires } from "../../compile/modules/update-requires";

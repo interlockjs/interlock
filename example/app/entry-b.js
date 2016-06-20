48faf515635714cc9567f8b8338cbe6b888a0176
Dale Bustad <dale@divmain.com>
2016-06-19 21:57:03 -0700

    Cosmetic changes.

--- example/app/entry-b.js
+++ example/app/entry-b.js
@@ -2,4 +2,4 @@ var libB = require("./shared/lib-b");
 import libC from "./shared/lib-c";
 
 console.log("entry-b:libB", libB);
-console.log("entry-b:libC", libC);
+console.log("entry-b:libC", libC.thing);
